ef943b95006322c865b2dd854a4c0fa603749bf9
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2013-07-21 00:31:14 +0200

    Retrieve data every 5m instead of 15m.

--- jobs/fitbit.rb
+++ jobs/fitbit.rb
@@ -5,8 +5,7 @@ date_format = "%H:%M"
 
 fitbit = Fitbit.new unit_system: unit_system, date_format: date_format
 
-SCHEDULER.every "15m", :first_in => 0 do |job|
-
+SCHEDULER.every "5m", :first_in => 0 do |job|
   send_event "fitbit", {
     device:   fitbit.device,
     steps:    fitbit.steps,
