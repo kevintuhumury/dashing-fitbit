66d163e64541ed9ebfa8821b2273a76cfd76fa78
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2013-09-01 22:43:35 +0200

    Let's use the 1.9 syntax.

--- jobs/fitbit.rb
+++ jobs/fitbit.rb
@@ -5,7 +5,6 @@ date_format = "%H:%M"
 
 fitbit = Fitbit.new unit_system: unit_system, date_format: date_format
 
-SCHEDULER.every "5m", :first_in => 0 do |job|
   send_event "fitbit", {
     device:   fitbit.device,
     steps:    fitbit.steps,
@@ -13,4 +12,5 @@ SCHEDULER.every "5m", :first_in => 0 do |job|
     distance: fitbit.distance,
     active:   fitbit.active
   }
+SCHEDULER.every "5m", first_in: 0 do |job|
 end
